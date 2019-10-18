8d11901aa0869d2c337e39cc2333bcce429b56a2
Yusuke Kuoka <ykuoka@gmail.com>
2019-10-18 11:50:25 +0900

    fix(exec): do not panic on too short summary

--- pkg/exec/exec.go
+++ pkg/exec/exec.go
@@ -166,9 +166,11 @@ func (c *Command) EnsureCheckRun(pre *github.PullRequestEvent) error {
 			desc = summary
 		}
 
-		// Otherwise you get errors like:
-		//  2019/10/17 18:25:08 Failed creating status: POST https://api.github.com/repos/variantdev/go-actions/statuses/ceb4320db3c54081d55daa6d7a50ed8dc7fafc86: 422 Validation Failed [{Resource:Status Field:description Code:custom Message:description is too long (maximum is 140 characters)}]
-		desc = desc[0:140]
+		if len(desc) > 140 {
+			// Otherwise you get errors like:
+			//  2019/10/17 18:25:08 Failed creating status: POST https://api.github.com/repos/variantdev/go-actions/statuses/ceb4320db3c54081d55daa6d7a50ed8dc7fafc86: 422 Validation Failed [{Resource:Status Field:description Code:custom Message:description is too long (maximum is 140 characters)}]
+			desc = desc[0:140]
+		}
 
 		status := &github.RepoStatus{
 			State:       github.String(state),
